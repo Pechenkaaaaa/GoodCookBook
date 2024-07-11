8f53abdf291e68568c1063a19b981cf892c877f2
Evgenij <pigorev2010@yandex.ru>
2024-07-11 09:47:43 +0300

    arch

---
+++ server/routes/index.routes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+// const catRoutes = require("./api/categoriy.routes");
+// const askRoutes = require("./api/ask.routes");
+
+// router.use("/category", catRoutes);
+// router.use("/ask", askRoutes);
+
+module.exports = router;
