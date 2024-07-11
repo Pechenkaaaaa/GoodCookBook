27a79552aced334388e462eaa15d5401613db02d
IgorPetrov-13 <petrov_igor_rf@mail.ru>
2024-07-11 11:25:38 +0300

    feat/db: done

---
+++ server/db/migrations/2-dishes-migration.js
@@ -0,0 +1,53 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Dishes", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      titel: {
+        type: Sequelize.STRING,
+      },
+      ingredients: {
+        type: Sequelize.TEXT,
+      },
+
+      ingredientsCount: {
+        type: Sequelize.INTEGER,
+      },
+      cookingTime: {
+        type: Sequelize.INTEGER,
+      },
+      img: {
+        type: Sequelize.TEXT,
+      },
+
+      // userId: {
+      //   type: Sequelize.INTEGER,
+      //   references: {
+      //     model: "Users",
+      //     key: "id"
+      //   },
+      //   onDelete: "CASCADE",
+      //   onUpdate: "CASCADE"
+      // },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Dishes");
+  },
+};
