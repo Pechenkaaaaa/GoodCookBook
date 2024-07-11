65f0e73f241992e792dda91113bb1e78a8b1f86e
unknown <benya0192@mail.ru>
2024-07-11 16:35:49 +0300

    2 seeds

--- server/db/seeders/dish.js
+++ server/db/seeders/dish.js
@@ -7,18 +7,18 @@ module.exports = {
       'Dishes',
       [
         {
-          titel: '1-Maserati quattroporte',
-          ingredients: 'Maserati quattroporte',
-          ingredientsCount: 8,
-          cookingTime: 30,
+          titel: 'Щи',
+          ingredients: 'Мясо, Капуста белокочанная, Картофель, Морковь, Лук репчатый, Томатная паста, Лавровый лист, Перец чёрный молотый, Соль, Масло растительное',
+          ingredientsCount: 10
+          cookingTime: 90,
           img: 'IMG#1',
           
         },
         {
-          titel: '2-Maserati quattroporte',
-          ingredients: 'Maserati quattroporte',
-          ingredientsCount: 8,
-          cookingTime: 30,
+          titel: 'Борщ',
+          ingredients: 'Говядина, Картофель, Капуста белокочанная, Свекла, Морковь, Лук репчатый, Томатная паста, Зелень петрушки, Соль, Масло растительное, Вода, Сметана ',
+          ingredientsCount: 12,
+          cookingTime: 110,
           img: 'IMG#2',
           
         },
