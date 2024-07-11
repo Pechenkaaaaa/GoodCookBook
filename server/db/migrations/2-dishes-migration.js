ca0c36379c5ff3718259ea7cb0a7ca5109254259
IgorPetrov-13 <petrov_igor_rf@mail.ru>
2024-07-11 16:36:22 +0300

    feat/client_main page Dish and Dishes added

--- server/db/migrations/2-dishes-migration.js
+++ server/db/migrations/2-dishes-migration.js
@@ -9,7 +9,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      titel: {
+      title: {
         type: Sequelize.STRING,
       },
       ingredients: {
--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -3,12 +3,14 @@ const router = require("express").Router();
 const userRouter = require('./api/user.routes');
 const authRouter = require('./api/auth.routes');
 const tokensRouter = require('./api/tokens.routes')
+const dishesRouter = require('./api/dishes.routes')
 // const catRoutes = require("./api/categoriy.routes");
 // const askRoutes = require("./api/ask.routes");
 
 router.use('/users', userRouter);
 router.use('/auth', authRouter);
 router.use('/tokens', tokensRouter);
+router.use('/dishes', dishesRouter);
 
 // router.use("/category", catRoutes);
 // router.use("/ask", askRoutes);
